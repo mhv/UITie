7fcab7aa039dded625856ca7734ffeadce1f1641
mhv <vroubel@gmail.com>
2016-08-15 22:47:46 +0300

    upgrade to swift 3

--- UITie/UITie.swift
+++ UITie/UITie.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Utils
+import Beholder
 
 @objc public class UITie : NSObject {
     public struct Consts {
@@ -28,31 +30,31 @@ import UIKit
     }
     static var ties:[String:(NSObject,String,NSObject,String,Mapper) -> Void] = [
         Consts.Get: {left, leftKey, right, rightKey, map in
-            if let x: AnyObject = map.to(right.valueForKeyPath(rightKey)) {
+            if let x: AnyObject = map.to(right.value(forKeyPath: rightKey)) {
                 left.setValue(x , forKeyPath: leftKey)
             }
         },
         Consts.Set: {right, rightKey, left, leftKey, map in // reverse args
-            if let x: AnyObject = map.to(right.valueForKeyPath(rightKey)) {
+            if let x: AnyObject = map.to(right.value(forKeyPath: rightKey)) {
                 left.setValue(x , forKeyPath: leftKey)
             }
         },
         Consts.Write: {left, leftKey, right, rightKey, map in
-            right.setValue(map.back(left.valueForKeyPath(leftKey)), forKeyPath: rightKey); ()
+            right.setValue(map.back(left.value(forKeyPath: leftKey)), forKeyPath: rightKey); ()
             weak var o = left.observeKeyPath(leftKey) { [weak right] x in
                 right?.setValue(map.back(x), forKeyPath: rightKey); ()
             }
-            right.observe {o?.cancel()}
+            _ = right.observe {o?.finish()}
         },
         Consts.Read: {right, rightKey, left, leftKey, map in // reverse args
-            right.setValue(map.to(left.valueForKeyPath(leftKey)), forKeyPath: rightKey); ()
+            right.setValue(map.to(left.value(forKeyPath: leftKey)), forKeyPath: rightKey); ()
             weak var o = left.observeKeyPath(leftKey) { [weak right] x in
                 right?.setValue(map.to(x), forKeyPath: rightKey); ()
             }
-            right.observe {o?.cancel()}
+            _ = right.observe {o?.finish()}
         },
         Consts.Bind: {left, leftKey, right, rightKey, map in
-            left.setValue(map.to(right.valueForKeyPath(rightKey)), forKeyPath: leftKey); ()
+            left.setValue(map.to(right.value(forKeyPath: rightKey)), forKeyPath: leftKey); ()
             weak var l = left.observeKeyPath(leftKey) { [weak right] x in
                 right?.setValue(map.back(x), forKeyPath: rightKey); ()
             }
@@ -65,7 +67,7 @@ import UIKit
             }
             _ = [left,right].map {
                 $0.observe {
-                    _ = [l,r].map {$0?.cancel()}
+                    _ = [l,r].map {$0?.finish()}
                 }
             }
         }
@@ -73,23 +75,23 @@ import UIKit
     var tied = false;
     func tie(leftKey:String,rightKey:String, map:Mapper) {
         if (!tied) {
-            if let right = self.left.source(rightKey, target: self.left) {
+            if let right = self.left.source(key: rightKey, target: self.left) {
                 tied = true;
                 UITie.ties[self.tieKey]!(self.left, leftKey,right, rightKey, map)
             }
         }
     }
-    public override func setValue(value: AnyObject?, forKeyPath keyPath: String) {
-        let rightKeys = (value as! NSString).componentsSeparatedByString(Consts.At)
+    public override func setValue(_ value: AnyObject?, forKeyPath keyPath: String) {
+        let rightKeys = (value as! NSString).components(separatedBy: Consts.At)
         let map = rightKeys.count == 2 ? Mapper.joinPath(rightKeys[0]) : Mapper()
         let rightKey = rightKeys.last!
-        tie(keyPath, rightKey: rightKey, map: map)
+        tie(leftKey: keyPath, rightKey: rightKey, map: map)
         if (!tied) {
-            left.observeKeyPath("tieSource", scope:self) {[weak self] _ in
-                self!.tie(keyPath, rightKey: rightKey, map: map)
+            _ = left.observeKeyPath("tieSource", scope:self) {[weak self] _ in
+                self!.tie(leftKey: keyPath, rightKey: rightKey, map: map)
             }
-            dispatch_async(dispatch_get_main_queue()) {
-                self.tie(keyPath, rightKey: rightKey, map: map)
+            DispatchQueue.main.async {
+                self.tie(leftKey: keyPath, rightKey: rightKey, map: map)
             }
         }
     }
@@ -98,9 +100,9 @@ import UIKit
 extension NSObject {
     @IBOutlet weak var tieSource:NSObject? {
         get{
-            return objc_getAssociatedObject(self, unsafeAddressOf(UITie.self)) as? NSObject
+            return objc_getAssociatedObject(self, unsafeAddress(of: UITie.self)) as? NSObject
         } set {
-            objc_setAssociatedObject(self, unsafeAddressOf(UITie.self), newValue, .OBJC_ASSOCIATION_ASSIGN)
+            objc_setAssociatedObject(self, unsafeAddress(of: UITie.self), newValue, .OBJC_ASSOCIATION_ASSIGN)
         }
     }
     public func source(key:String, target:NSObject) -> NSObject? {
@@ -122,24 +124,24 @@ extension NSObject {
 
 extension UIResponder {
     override public func source(key: String, target: NSObject) -> NSObject? {
-        return (tieSource ?? nextResponder())?.source(key, target: target);
+        return (tieSource ?? next)?.source(key: key, target: target);
     }
 }
 
 extension UIViewController {
     override public func source(key: String, target: NSObject) -> NSObject? {
-        return tieSource?.source(key, target: target) ?? self
+        return tieSource?.source(key: key, target: target) ?? self
     }
 }
 
 extension UITableViewCell {
     override public func source(key: String, target: NSObject) -> NSObject? {
-        return tieSource?.source(key, target: target) ?? self
+        return tieSource?.source(key: key, target: target) ?? self
     }
 }
 
 extension UICollectionViewCell {
     override public func source(key: String, target: NSObject) -> NSObject? {
-        return tieSource?.source(key, target: target) ?? self
+        return tieSource?.source(key: key, target: target) ?? self
     }
 }
